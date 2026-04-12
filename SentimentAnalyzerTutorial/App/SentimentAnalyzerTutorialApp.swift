f8c029940bc152ed070fa39560434812c26000ca
Sachin Randive <r.sachin81@gmail.com>
2026-04-12 21:58:54 +0530

    Initial Folder created for base set up.

---
+++ SentimentAnalyzerTutorial/App/SentimentAnalyzerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalyzerTutorialApp.swift
+//  SentimentAnalyzerTutorial
+//
+//  Created by Sachin Randive on 12/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalyzerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
